5a5dd9d68046b1bbb57823cf3df2be350e3d8829
Brett Smith <brett@codeq.com>
2017-01-07 11:04:16 -0600

    add yarn package manager & clean tabbing

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-var gulp 					= require('gulp'),
-		sass 					= require('gulp-sass'),
-		autoprefixer 	= require('gulp-autoprefixer'),
-		browserSync 	= require('browser-sync').create(),
-		useref 				= require('gulp-useref'),
+var gulp					= require('gulp'),
+		sass					= require('gulp-sass'),
+		autoprefixer	= require('gulp-autoprefixer'),
+		browserSync		= require('browser-sync').create(),
+		useref				= require('gulp-useref'),
 		uglify 				= require('gulp-uglify'),
-		gulpif 				= require('gulp-if'),
-		runSequence 	= require('run-sequence'),
+		gulpif				= require('gulp-if'),
+		runSequence		= require('run-sequence'),
 		plumber				= require('gulp-plumber');
 
-var onError = function (err) {
+function onError(err) {
   console.log(err);
 };
 
@@ -33,15 +33,15 @@ gulp.task('sass', function(){
 })
 
 gulp.task('browserSync', function(){
-	 browserSync.init({
-        server: "./app"
-    });
+	browserSync.init({
+    server: "./app"
+	});
 })
 
 gulp.task('watch', ['browserSync', 'sass'], function(){
 	gulp.watch('app/assets/scss/**/*.scss', ['sass']);
 	gulp.watch('app/*.html', browserSync.reload);
-  gulp.watch('app/assets/js/**/*.js', browserSync.reload);
+	gulp.watch('app/assets/js/**/*.js', browserSync.reload);
 })
 
 
@@ -50,29 +50,33 @@ gulp.task('watch', ['browserSync', 'sass'], function(){
 
 gulp.task('useref', function() {
 
-  return gulp.src('app/*.html')
-    .pipe(useref())
-    .pipe(gulpif('*.js', uglify()))
-    .pipe(gulp.dest('dist'))
+	return gulp.src('app/*.html')
+  	.pipe(useref())
+		.pipe(gulpif('*.js', uglify()))
+		.pipe(gulp.dest('dist'))
 })
 
 gulp.task('move', function() {
-  return gulp.src(['app/assets/**', '!app/assets/{js,js/**}', '!app/assets/{scss,scss/**}'])
-  .pipe(gulp.dest('dist/assets'))
+	return gulp.src(['app/assets/**', '!app/assets/{js,js/**}', '!app/assets/{scss,scss/**}'])
+  	.pipe(gulp.dest('dist/assets'))
 })
 
 
-// Build Sequences
+// Gulp
 // ---------------
 
-gulp.task('build', function (callback) {
-	runSequence(['sass', 'useref', 'move'],
+gulp.task('default', function (callback) {
+	runSequence(['sass', 'browserSync', 'watch'],
 		callback
 	)
 })
 
-gulp.task('default', function (callback) {
-	runSequence(['sass', 'browserSync', 'watch'],
+
+// Gulp Build
+// ---------------
+
+gulp.task('build', function (callback) {
+	runSequence(['sass', 'useref', 'move'],
 		callback
 	)
 })
